4bcfc120405f21c3ac41a83f576b75c38732a4b8
范圣刚 <princetoad@gmail.com>
2017-09-01 16:30:55 +0800

    json schema 模板更新

---
+++ generators/app/templates/test/specs/api.spec.js
@@ -0,0 +1,146 @@
+// 测试框架
+//
+const { test } = require('ava'); // 测试用例对象
+const { expect } = require('chai'); // BDD 断言库
+
+// 测试辅助
+//
+const faker = require('faker/locale/zh_CN'); // fake 数据
+const { login } = require('../helpers/login'); // 登录接口
+
+// 读取配置
+//
+const { accountNo, userName, password, newPassword, setting } = require('../helpers/global-config'); // 在全局配置里面配置的测试数据
+
+// 引入 service 方法
+//
+const {
+  get<%=modelPluralUpperFirstName%>,
+  get<%=modelUpperFirstName%>,
+  make<%=modelUpperFirstName%>,
+  post<%=modelUpperFirstName%>,
+  put<%=modelUpperFirstName%>,
+  delete<%=modelUpperFirstName%>
+} = require('./service');
+
+// 需要用到的其他模块的方法
+//
+// const { thawTellers, cancelTellers, queryInsTellersByPage } = require('../teller-admin/service.js');
+
+/**
+ * 接口测试之前需要先登录
+ */
+test.before('登录', async t => {
+  try {
+    const resultCode = await login({ userName, password });
+    if (resultCode !== '00000') {
+      await login({ userName, password: newPassword });
+    }
+
+    t.pass();
+  } catch (err) {
+    console.error(err);
+    t.fail(err);
+  }
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// 测试用例
+//
+//-------------------------------------<%=modelUpperFirstName%>-----------------------------------------------//
+test('查询全部<%=modelUpperFirstName%>信息', async t => {
+  try {
+    const response = await get<%=modelPluralUpperFirstName%>()
+    const <%=modelPluralCamelName%> = response.data.resultData
+    expect(<%=modelPluralCamelName%>).to.be.not.empty
+
+    t.pass()
+  } catch (err) {
+    console.error(err)
+    t.fail(err)
+  }
+})
+
+test('查询<%=moduleName%>详细信息', async t => {
+  try {
+    let response = await get<%=modelPluralUpperFirstName%>()
+    const <%=modelPluralCamelName%> = response.data.resultData
+    if (<%=modelPluralCamelName%> && <%=modelPluralCamelName%>.length) {
+      const entityId = faker.random.arrayElement(<%=modelPluralCamelName%>.map(item => item.id))
+      response = await get<%=modelUpperFirstName%>(entityId)
+    }
+
+    t.pass()
+  } catch (err) {
+    console.error(err)
+    t.fail(err)
+  }
+})
+
+/**
+ * 添加<%=modelUpperFirstName%>
+ */
+test('添加<%=modelUpperFirstName%>信息', async t => {
+  try {
+    const <%=modelName%> = make<%=modelUpperFirstName%>()
+    response = await post<%=modelUpperFirstName%>(<%=modelName%>)
+    const id = response.data.resultData
+    response = await get<%=modelUpperFirstName%>(id)
+    expect(response.data.resultData).to.deep.equal(<%=modelName%>)
+
+    // 插完记得删除
+    if (setting.clearAuto) {
+      response = await delete<%=modelUpperFirstName%>(id)
+    }
+
+    t.pass()
+  } catch (err) {
+    console.error(err)
+    t.fail(err)
+  }
+})
+
+test('修改<%=moduleName%>信息', async t => {
+  try {
+    let response = await get<%=modelPluralUpperFirstName%>()
+    const <%=modelPluralCamelName%> = response.data.resultData
+    let iCount = 0
+    while (iCount < <%=modelPluralCamelName%>.length) {
+      const <%=modelName%> = <%=modelPluralCamelName%>[iCount]
+      const entity = make<%=modelUpperFirstName%>({
+      })
+      response = await put<%=modelUpperFirstName%>(entity)
+      response = await get<%=modelUpperFirstName%>(<%=modelName%>.id)
+
+      expect(response.data.resultData).to.deep.equal(entity)
+
+      iCount += 1
+    }
+
+    t.pass()
+  } catch (err) {
+    console.error(err)
+    t.fail(err)
+  }
+})
+
+test('删除<%=modelUpperFirstName%>信息', async t => {
+  try {
+    let response = await get<%=modelPluralUpperFirstName%>()
+    const <%=modelPluralCamelName%> = response.data.resultData
+    if (<%=modelPluralCamelName%>.length === 0) {
+      return Promise.reject(new Error('没有<%=modelUpperFirstName%>!'))
+    }
+
+    const <%=modelName%> = faker.random.arrayElement(<%=modelPluralCamelName%>)
+    await delete<%=modelUpperFirstName%>(<%=modelName%>)
+    response = await get<%=modelPluralUpperFirstName%>()
+    const <%=modelName%>Ids = response.data.resultData.map(item => item.id)
+    expect(<%=modelName%>.id).to.not.be.oneOf(<%=modelName%>Ids)
+
+    t.pass()
+  } catch (err) {
+    console.error(err)
+    t.fail(err)
+  }
+})
---
+++ generators/app/templates/test/specs/service.js
@@ -0,0 +1,160 @@
+const {expect} = require('chai') // BDD 断言库
+const chai = require('chai')
+chai.use(require('chai-json-schema')) // json schema 的 chai.js 插件
+
+// 测试辅助库和方法
+//
+const {cloneDeep} = require('lodash') // 深度克隆测试请求数据
+const moment = require('moment') // 日期时间操作辅助库
+const faker = require('faker/locale/zh_CN') // fake 数据
+
+const request = require('../helpers/request') // 使用 axios 发起 RESTful 请求
+const {expectOK, expectStatusAndMessage, expectStatusOK, expectStatusNotOK} = require('../helpers/expect-http') // 封装的 http 层面的断言方法
+const {distillInfo, randomColor} = require('../helpers/distill') // 通用信息控制台打印方法
+const {accountNo, userName, password, newPassword, setting} = require('../helpers/global-config') // 在全局配置里面配置的测试数据
+const {login} = require('../helpers/login') // 登录接口
+const {dateTimeLong, dateTimeShort} = require('../helpers/datetime') // 日期时间格式配置
+
+// institution JSON Schema
+const CreateRequestSchema = require('./create-request.json')
+const CreateResponseSchema = require('./create-response.json')
+const RetrieveRequestSchema = require('./retrieve-request.json')
+const RetrieveResponseSchema = require('./retrieve-response.json')
+const UpdateRequestSchema = require('./update-request.json')
+const UpdateResponseSchema = require('./update-response.json')
+const DeleteRequestSchema = require('./delete-request.json')
+const DeleteResponseSchema = require('./delete-response.json')
+const ListRequestSchema = require('./list-request.json')
+const ListResponseSchema = require('./list-response.json')
+
+//
+// <%=modelUpperFirstName%>
+//
+/**
+ * 查询<%=modelUpperFirstName%>列表
+ */
+const get<%=modelPluralUpperFirstName%> = async () => {
+  try {
+    // 没有请求数据, 就不需要校验. 如果支持分页的话, 起码需要校验分页字段, 如果还有查询条件的话, 同时写到 request 的 schema 里面
+    const response = await request.post('/<%=moduleName%>/query<%=modelPluralUpperFirstName%>')
+    distillInfo(response)
+    expect(response.data).to.be.jsonSchema(ListResponseSchema)
+    expectStatusOK(response)
+
+    return Promise.resolve(response)
+  } catch (err) {
+    console.error(err)
+    return Promise.reject(err)
+  }
+}
+
+/**
+ * 查询<%=modelUpperFirstName%>详情
+ */
+const get<%=modelUpperFirstName%> = async (id) => {
+  try {
+    const data = {id}
+    expect(data).to.be.jsonSchema(RetrieveRequestSchema)
+    const response = await request.post('/<%=moduleName%>/query<%=modelUpperFirstName%>', data)
+    distillInfo(response)
+    expect(response.data).to.be.jsonSchema(RetrieveResponseSchema)
+    expectStatusOK(response)
+
+    return Promise.resolve(response)
+  } catch (err) {
+    console.error(err)
+    return Promise.reject(err)
+  }
+}
+
+/**
+ * 构造<%=modelUpperFirstName%>实例
+ */
+const make<%=modelUpperFirstName%> = ({id, parentId, insCode, insName}) => {
+  const data = {
+    insCode: insCode || `${faker.lorem.words(2)} ${faker.random.number(1000)}`,
+    insName: insName || `ins-${faker.commerce.department()}-${faker.commerce.department()}`,
+    isTrading: faker.random.arrayElement([0, 1]),
+    parentId,
+    contacts: `${faker.name.firstName()}${faker.name.lastName()}`,
+    telephone: faker.phone.phoneNumber('139########'),
+    zipCode: faker.address.zipCode(),
+    fax: faker.phone.phoneNumber('010-########'),
+    address: `${faker.address.state()}${faker.address.city()}${faker.address.streetAddress()}`
+  }
+  if (id || id === 0) {
+    data.id = id
+  }
+  return data
+}
+
+/**
+ * 保存<%=modelUpperFirstName%>实例
+ * 发送请求前校验请求数据,收到应答后校验应答数据.
+ * @param entity
+ * @returns {Promise.<*>}
+ */
+const post<%=modelUpperFirstName%> = async (entity) => {
+  try {
+    expect(entity).to.be.jsonSchema(CreateRequestSchema)
+    const response = await request.post('/<%=moduleName%>/save<%=modelUpperFirstName%>', entity)
+    distillInfo(response)
+    expect(response.data).to.be.jsonSchema(CreateResponseSchema)
+    expectStatusOK(response)
+
+    return Promise.resolve(response)
+  } catch (err) {
+    console.log(err)
+    return Promise.reject(err)
+  }
+}
+
+/**
+ * 更新<%=modelUpperFirstName%>实例
+ * @param entity
+ * @returns {Promise.<*>}
+ */
+const put<%=modelUpperFirstName%> = async (entity) => {
+  try {
+    expect(entity).to.be.jsonSchema(UpdateRequestSchema)
+    const response = await request.post('/<%=moduleName%>/modify<%=modelUpperFirstName%>', entity)
+    distillInfo(response)
+    expect(response.data).to.be.jsonSchema(UpdateResponseSchema)
+    expectStatusOK(response)
+
+    return Promise.resolve(response)
+  } catch (err) {
+    console.log(err)
+    return Promise.reject(err)
+  }
+}
+
+/**
+ * 删除<%=modelUpperFirstName%>实例
+ * @param id
+ * @returns {Promise.<*>}
+ */
+const delete<%=modelUpperFirstName%> = async (id) => {
+  try {
+    const data = {id}
+    expect(data).to.be.jsonSchema(DeleteRequestSchema)
+    const response = await request.post('/<%=moduleName%>/delete<%=modelUpperFirstName%>', data)
+    distillInfo(response)
+    expect(response.data).to.be.jsonSchema(DeleteResponseSchema)
+    expectStatusOK(response)
+
+    return Promise.resolve(response)
+  } catch (err) {
+    console.log(err)
+    return Promise.reject(err)
+  }
+}
+
+module.exports = {
+  get<%=modelPluralUpperFirstName%>,
+  get<%=modelUpperFirstName%>,
+  make<%=modelUpperFirstName%>,
+  post<%=modelUpperFirstName%>,
+  put<%=modelUpperFirstName%>,
+  delete<%=modelUpperFirstName%>
+}
