b9594f561095ccb325da48ae1230e9473cb17b01
范圣刚 <princetoad@gmail.com>
2017-09-02 18:01:21 +0800

    增加对 jsonSchema 的 ejs 编译测试

--- test/ejs.spec.js
+++ test/ejs.spec.js
@@ -49,5 +49,66 @@ test('ko-script.js', t => {
     t.pass()
   } catch (err) {
     console.error(err)
+    t.fail(err)
+  }
+})
+
+test('test/specs/create-request.json', t => {
+  try {
+    const jsonSchemas = [
+      'create-request.json',
+      'create-response.json',
+      'retrieve-request.json',
+      'retrieve-response.json',
+      'update-request.json',
+      'update-response.json',
+      'delete-request.json',
+      'delete-response.json',
+      'list-request.json',
+      'list-response.json'
+    ]
+    jsonSchemas.forEach(item => {
+      const script = fs.readFileSync(resolve(__dirname, `../generators/app/templates/test/specs/${item}`), {encoding: 'utf8'})
+      const options = {
+        moduleName: 'customer-list',
+        modelName: 'Customer',
+        moduleTitle: '客户信息查询',
+        formFields: 'certName:string:姓名 mobile:string:手机号 certNo:string:身份证号 accountNo:string:二类户账号'.trim().split(/\s/),
+        attrs: 'custNo:string:客户号 accountNo:string:二类户账号 certName:string:姓名 mobile:string:手机号 certNo:string:证件号码 certType:string:证件类型'.trim().split(/\s/),
+        actionTypes: ['includeCreate', 'includeRetrieve', 'includeUpdate', 'includeDelete']
+      }
+      const attrMapping = attr => ({
+        name: camelCase(attr.split(':')[0]), // 属性使用 camelCase, constraintType
+        kebabAttrName: kebabCase(attr.split(':')[0]), // constraint-type
+        upperFirstAttrName: upperFirst(camelCase(attr.split(':')[0])), // ConstraintType
+        chosenAttrName: `chosen${upperFirst(camelCase(attr.split(':')[0]))}`, // chosenConstraintType
+        availableAttrName: `available${pluralize(upperFirst(camelCase(attr.split(':')[0])))}`, // availableConstraintTypes
+        type: toLower(attr.split(':')[1]) || 'string', // 数据类型: string, integer, datetime, daterange etc.
+        label: attr.split(':')[2] || camelCase(attr.split(':')[0])
+      })
+      const formFields = options.formFields.map(attrMapping)
+      const attrs = options.attrs.map(attrMapping)
+      const model = {
+        moduleName: kebabCase(options.moduleName),
+        modelName: options.modelName,
+        modelKebabName: kebabCase(options.modelName),
+        modelPluralCamelName: pluralize(camelCase(options.modelName)),
+        modelPluralUpperFirstName: upperFirst(pluralize(camelCase(options.modelName))),
+        modelCurrentName: `current${upperFirst(camelCase(options.modelName))}`,
+        modelUpperFirstName: upperFirst(camelCase(options.modelName)),
+        moduleTitle: options.moduleTitle,
+        actionTypes: options.actionTypes,
+        formFields,
+        attrs
+      }
+      console.log('model = ', model)
+      const renderText = template.render(script, model)
+      expect(renderText).to.exist
+    })
+
+    t.pass()
+  } catch (err) {
+    console.error(err)
+    t.fail(err)
   }
 })
